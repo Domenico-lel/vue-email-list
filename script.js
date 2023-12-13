412ddccb0a1df1efe295ef3b9a9afa0b00335bce
Domenico-lel <domenicolella2@gmail.com>
2023-12-13 20:09:27 +0100

    creato script

--- script.js
+++ script.js
@@ -2,14 +2,27 @@ const { createApp } = Vue;
 
 createApp({
     data() {
-        return{
+        return {
+            emails: [] // Array per immagazzinare le email
+        };
+    },
+    methods: {
+        fetchEmails() {
+            const numEmails = 10;
+            const apiUrl = `https://jsonplaceholder.typicode.com/comments?_limit= ${numEmails}`;
 
+            // Effettua la richiesta API usando axios
+            axios.get(apiUrl)
+                .then(response => {
+                    // Aggiorna l'array delle email con i dati ottenuti
+                    this.emails = response.data.map(item => item.email);
+                })
+                .catch(error => {
+                    console.error('Errore nella richiesta API:', error);
+                });
         }
     },
-    methods:{
-
-    },
     mounted() {
-        console.log("l'applicazione è carcata!!")
+        this.fetchEmails(); // Chiama la funzione fetchEmails quando l'app viene montata
     }
-})
+}).mount('#app');
